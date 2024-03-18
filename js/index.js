7f8b210282ca7453063a7266fcc1473c9c65bac3
ali <alihgad2@gmail.com>
2024-03-18 03:33:13 +0200

    add ecommerce

--- js/index.js
+++ js/index.js
@@ -1,109 +1,109 @@
 
 let container = [
     {
-        link : "https://alihgad.github.io/kasper/",
-        image : "./imges/Kasper.png",
-        alt : "Kasper",
-        h4 :  "Kasper template",
-        tech :  "native html + css , responsive" , 
-        date : "oct-2022"
+        link: "https://alihgad.github.io/kasper/",
+        image: "./imges/Kasper.png",
+        alt: "Kasper",
+        h4: "Kasper template",
+        tech: "native html + css , responsive",
+        date: "oct-2022"
     }
     ,
     {
-        link : "https://alihgad.github.io/Leon/",
-        image : "./imges/leon.png",
-        alt : "leon",
-        h4 : "leon template",
-        tech : "native html + css , responsive" , 
-        date : "nov-2022"
+        link: "https://alihgad.github.io/Leon/",
+        image: "./imges/leon.png",
+        alt: "leon",
+        h4: "leon template",
+        tech: "native html + css , responsive",
+        date: "nov-2022"
     },
     {
-        link : "https://alihgad.github.io/mealify/",
-        image : "./imges/mealify.png",
-        alt : "mealify",
-        h4 : "mealify template",
-        tech : "native html + css , responsive , dark mood" , 
-        date : "oct-2023"
+        link: "https://alihgad.github.io/mealify/",
+        image: "./imges/mealify.png",
+        alt: "mealify",
+        h4: "mealify template",
+        tech: "native html + css , responsive , dark mood",
+        date: "oct-2023"
     },
     {
-        link : "https://alihgad.github.io/fokir/",
-        image :"./imges/fokir.png",
-        alt : "fokir",
-        h4 : "fokir template",
-        tech : "native html + css , responsive" , 
-        date : "oct-2023"
+        link: "https://alihgad.github.io/fokir/",
+        image: "./imges/fokir.png",
+        alt: "fokir",
+        h4: "fokir template",
+        tech: "native html + css , responsive",
+        date: "oct-2023"
     },
     {
-        link : "https://alihgad.github.io/DevFolio/",
-        image :"./imges/devfolio.png",
-        alt : "devfolio",
-        h4 : "devfolio template",
-        tech : "html + css + bootstrap , responsive" , 
-        date : "nov-2023"
+        link: "https://alihgad.github.io/DevFolio/",
+        image: "./imges/devfolio.png",
+        alt: "devfolio",
+        h4: "devfolio template",
+        tech: "html + css + bootstrap , responsive",
+        date: "nov-2023"
     },
     {
-        link : "https://alihgad.github.io/bezel/",
-        image :"./imges/bezel.png",
-        alt : "bezel",
-        h4 : "bezel template",
-        tech : "html + css + bootstrap , responsive" , 
-        date : "nov-2023"
+        link: "https://alihgad.github.io/bezel/",
+        image: "./imges/bezel.png",
+        alt: "bezel",
+        h4: "bezel template",
+        tech: "html + css + bootstrap , responsive",
+        date: "nov-2023"
     },
     {
-        link : "https://alihgad.github.io/CRUDS/",
-        image :"./imges/cruds.png",
-        alt : "CRUDS",
-        h4 : "CRUDS system",
-        tech : "html + css + bootstrap , JavaScript" , 
-        date : "Dec-2023"
+        link: "https://alihgad.github.io/CRUDS/",
+        image: "./imges/cruds.png",
+        alt: "CRUDS",
+        h4: "CRUDS system",
+        tech: "html + css + bootstrap , JavaScript",
+        date: "Dec-2023"
     },
     {
-        link : "https://alihgad.github.io/bookmark/",
-        image :"./imges/bookmark.png",
-        alt : "bookmark",
-        h4 : "bookmark system",
-        tech : "html + css + bootstrap , JavaScript" , 
-        date : "Dec-2023"
+        link: "https://alihgad.github.io/bookmark/",
+        image: "./imges/bookmark.png",
+        alt: "bookmark",
+        h4: "bookmark system",
+        tech: "html + css + bootstrap , JavaScript",
+        date: "Dec-2023"
     },
     {
-        link : "https://alihgad.github.io/smartLogIn/",
-        image :"./imges/login.png",
-        alt : "smartLogIn",
-        h4 : "LogIn system",
-        tech : "html + css + bootstrap , JavaScript" , 
-        date : "Dec-2023"
+        link: "https://alihgad.github.io/smartLogIn/",
+        image: "./imges/login.png",
+        alt: "smartLogIn",
+        h4: "LogIn system",
+        tech: "html + css + bootstrap , JavaScript",
+        date: "Dec-2023"
     },
     {
-        link : "https://alihgad.github.io/weather-App/",
-        image :"./imges/weather.png",
-        alt : "weather-App",
-        h4 : "weather App",
-        tech : "html + css + bootstrap , JavaScript" , 
-        date : "Dec-2023"
+        link: "https://alihgad.github.io/weather-App/",
+        image: "./imges/weather.png",
+        alt: "weather-App",
+        h4: "weather App",
+        tech: "html + css + bootstrap , JavaScript",
+        date: "Dec-2023"
     },
     {
-        link : "https://alihgad.github.io/ToDoList/",
-        image :"./imges/todolist.png",
-        alt : "ToDo list",
-        h4 : "ToDo list",
-        tech : "html + css + bootstrap + sweetalert2 , JavaScript" , 
-        date : "Jan-2024"
+        link: "https://alihgad.github.io/ToDoList/",
+        image: "./imges/todolist.png",
+        alt: "ToDo list",
+        h4: "ToDo list",
+        tech: "html + css + bootstrap + sweetalert2 , JavaScript",
+        date: "Jan-2024"
     },
     {
-        link : "https://alihgad.github.io/Games/",
-        image :"./imges/games.png",
-        alt : "Games reviews",
-        h4 : "Games reviews",
-        tech : "html + css + bootstrap  , JavaScript + API" , 
-        date : "Jan-2024"
+        link: "https://alihgad.github.io/Games/",
+        image: "./imges/games.png",
+        alt: "Games reviews",
+        h4: "Games reviews",
+        tech: "html + css + bootstrap  , JavaScript + API",
+        date: "Jan-2024"
     },
     {
-        link : "https://alihgad.github.io/party/",
-        image :"./imges/party.png",
-        alt : "Party Event",
-        h4 : "Party Event",
-        tech : "html + css + bootstrap  , JavaScript + JQuery" , 
-        date : "Jan-2024"
+        link: "https://alihgad.github.io/party/",
+        image: "./imges/party.png",
+        alt: "Party Event",
+        h4: "Party Event",
+        tech: "html + css + bootstrap  , JavaScript + JQuery",
+        date: "Jan-2024"
     }
 ]
 
@@ -111,10 +111,10 @@ let container = [
 
 
 function displayProjects() {
- 
+
     let cartona = ""
-    for(let i=container.length -1;i>=0;i--){
-        cartona +=  `<div class="col  ">
+    for (let i = container.length - 1; i >= 0; i--) {
+        cartona += `<div class="col  ">
                         <a class="text-decoration-none" href=${container[i].link} target="_blank">
                             <div class="card text-capitalize shadow-3d ">
                             <div class="overflow-hidden rounded-top-2">
@@ -136,26 +136,35 @@ function displayProjects() {
                         </a>
                     </div>`
     }
-    document.querySelector("#projects").innerHTML=cartona;
+    document.querySelector("#projects").innerHTML = cartona;
 }
 
 
 
 let reactContainer = [{
-    link : "https://ali-react-app.netlify.app/",
-        image :"./imges/start.png",
-        alt : "start",
-        h4 : "first react app",
-        tech : "html + css + bootstrap  , JavaScript React Js" , 
-        date : "Feb-2024"
-}
+    link: "https://ali-react-app.netlify.app/",
+    image: "./imges/start.png",
+    alt: "start",
+    h4: "first react app",
+    tech: "html + css + bootstrap  , JavaScript React Js",
+    date: "Feb-2024"
+},
+{
+    link: "https://alifreshcart.000webhostapp.com/",
+    image: "./imges/start.png",
+    alt: "fresh cart",
+    h4: "E-commerce",
+    tech: "html + css + bootstrap  , JavaScript React Js",
+    date: "mar-2024"
+},
+
 ]
 
 function displayReactProjects() {
- 
+
     let cartona = ""
-    for(let i= reactContainer.length-1 ;i >= 0   ;i--){
-        cartona +=  `<div class="col  ">
+    for (let i = reactContainer.length - 1; i >= 0; i--) {
+        cartona += `<div class="col  ">
                         <a class="text-decoration-none" href=${reactContainer[i].link} target="_blank">
                             <div class="card text-capitalize shadow-3d ">
                             <div class="overflow-hidden rounded-top-2">
@@ -177,12 +186,12 @@ function displayReactProjects() {
                         </a>
                     </div>`
     }
-    document.querySelector("#react").innerHTML=cartona;
+    document.querySelector("#react").innerHTML = cartona;
 }
 
 
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     displayProjects()
     displayReactProjects()
 })
